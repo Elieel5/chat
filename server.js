9b8ff76db7a27c617e04213d471da2ba9f2eec8d
Eliel Ribeiro <elielribeiro088@gmail.com>
2022-05-09 23:57:00 -0300

    feat: chat

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const PORT = 3000;
+const SocketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = SocketIO(server);
 
-
-server.listen(PORT);
+server.listen(3000);
 app.use(express.static(path.join(__dirname, 'public')));
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+  console.log('connection');
+
+  socket.on('join-request', (username) => {
+    socket.username = username;
+    connectedUsers.push( username );
+    console.log( username );
+
+    socket.emit('user-ok', connectedUsers);
+    socket.broadcast.emit('list-update',{
+      joined: username,
+      list: connectedUsers
+    });
+  });
+
+  socket.on('disconnect', () => {
+    connectedUsers = connectedUsers.filter(u => u != socket.username);
+    console.log(connectedUsers);
+    
+    socket.broadcast.emit('list-update', {
+      left: socket.username,
+      list: connectedUsers
+    });
+  });
+});
 
 
